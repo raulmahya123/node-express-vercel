019890c9a86c5b9fc450e2fe51d4b7a43506e6eb
Mahyaaa <94241597+raulmahya123@users.noreply.github.com>
2024-12-08 19:33:00 +0700

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
     title: "Express Testing",
-    message: "HALOOOO GUYS",
+    message: "IQBAL EWEEEE",
   });
 });
 
